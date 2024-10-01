c0ae932877e01069f12bb1de1e0ccc715e95c39f
Nicolai Grymer <pronicoxd@gmail.com>
2024-10-01 12:47:53 +0200

    Improved ControlPanel UI

--- src/controls/ControlPanel.tsx
+++ src/controls/ControlPanel.tsx
@@ -3,33 +3,37 @@ import BuyStock from "./BuyStock";
 import SellStock from "./SellStock";
 import SeeTransactions from "./SeeTransactions";
 import AdminPanel from "../admin/AdminPanel";
-import { Settings } from "lucide-react";
+import { ArrowLeftRight, Settings } from "lucide-react";
 
 export default function ControlPanel() {
   return (
-    <div className="ml-auto flex justify-between w-full">
-      <div className="flex gap-2">
+    <div className="absolute bottom-0 w-screen left-0 flex justify-center">
+      <div className="flex gap-2 items-end px-8 py-4 rounded-t-xl bg-card border">
+        <SeeTransactions>
+          <Button variant="outline" size="sm">
+            <ArrowLeftRight size={16} className="mr-2" />
+            Transactions
+          </Button>
+        </SeeTransactions>
         <BuyStock>
           <Button
             variant="secondary"
-            className=" hover:bg-green-700 px-8 bg-green-600"
+            size="lg"
+            className=" hover:bg-green-700 px-8 bg-green-600 text-lg"
           >
             Buy
           </Button>
         </BuyStock>
         <SellStock>
-          <Button variant="destructive" className="px-8">
+          <Button size="lg" variant="destructive" className="px-8 text-lg">
             Sell
           </Button>
         </SellStock>
-      </div>
-      <div className="flex gap-2">
-        <SeeTransactions>
-          <Button variant="outline">Transactions</Button>
-        </SeeTransactions>
+
         <AdminPanel>
-          <Button variant="outline" size="icon">
-            <Settings />
+          <Button variant="outline" size="sm">
+            <Settings size={16} className="mr-2" />
+            Settings
           </Button>
         </AdminPanel>
       </div>
